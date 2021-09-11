ff9764eb5cf274c5afd7c1dd8dd283c37f580736
chanon-mike <zzmikezz8520@gmail.com>
2021-09-11 20:17:42 +0700

    Fix headers

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ params = {
     "acl:consumerKey": API_KEY
 }
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"}
-response = requests.get(ENDPOINT + data_type, params=params)
+response = requests.get(ENDPOINT + data_type, params=params, headers=headers)
 print(response)
 data = response.json()
 
