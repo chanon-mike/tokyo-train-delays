1355991e3aeb901d62e95783fe12d6d877130ff8
chanon-mike <zzmikezz8520@gmail.com>
2021-09-26 00:03:03 +0700

    Add Japanese language to train status table
    Change the format of train_status dictionary

--- main.py
+++ main.py
@@ -24,6 +24,26 @@ def request_data(rdf_type):
 
     return data
 
+railway_data = request_data("odpt:Railway")
+
+def get_railway(railway, language):
+    for info in railway_data:
+        railway_name = info["owl:sameAs"].split(":")[-1]
+        # e.g. Keisei, Seibu, Keikyu exception
+        if "." not in railway:
+            railway_name = railway_name.split(".")[0]
+        if railway == railway_name:
+            return info["odpt:railwayTitle"][language]
+
+operator_data = request_data("odpt:Operator")
+
+def get_operator(operator, language):
+    for info in operator_data:
+        operator_name = info["owl:sameAs"].split(":")[-1]
+        if operator == operator_name:
+            return info["odpt:operatorTitle"][language]
+
+
 def camel_case_split(str):
     return " ".join(re.sub('([a-z])([A-Z])', r'\1 \2', str).split())
 
@@ -36,23 +56,31 @@ def home():
     railways = [information['owl:sameAs'].split(':')[1].split(".") for information in data]
     operators = list(set([railway[0] for railway in railways]))
     train_dict = {}
-
+    
     # Loop through operators and railways to create dictionary of information
     for i in range(len(operators)):
         train_status = [information['odpt:trainInformationText']['ja'] for information in data if information['owl:sameAs'].split(':')[1].split(".")[0] == operators[i]]
         time = [":".join(information['dc:date'].split('T')[1].split('+')[0].split(":")[:2]) for information in data if information['owl:sameAs'].split(':')[1].split(".")[0] == operators[i]]
-        # Split the camel case data to normal format
-        operators[i] = camel_case_split(operators[i])
         train_dict.update(
             {
-                operators[i]: {
-                    "railways": [camel_case_split(railway[-1]) for railway in railways if camel_case_split(railway[0]) == operators[i]],
-                    "train_status": train_status,
-                    "time": time
-                }
+                camel_case_split(operators[i]): 
+                {
+                    "ja": {
+                        "operator": get_operator(operators[i], "ja"),
+                        "railways": [get_railway(".".join(railway), "ja") for railway in railways if railway[0] == operators[i]],
+                        "train_status": train_status,
+                        "time": time
+                    },
+                    "en": {
+                        "operator": get_operator(operators[i], "en"),
+                        "railways": [camel_case_split(railway[-1]) for railway in railways if railway[0] == operators[i]],
+                        "train_status": train_status,
+                        "time": time
+                    }
+                } 
             }
         )
-
+    
     return render_template("index.html", train_dict=train_dict)
 
 
@@ -85,7 +113,12 @@ def passenger():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
+        
+    # from pprint import pprint
+    # for operator in list(train_dict.keys()):
+    #     pprint(len(train_dict[operator]['ja']['railways'])+1)
+        # for i in range(len(list(train_dict[operator]["ja"]["railways"]))):
+        #     print(train_dict[operator]["ja"]["railways"][i])
 
 
     
